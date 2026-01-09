66b2883bef76cd96f3ff53caae5b5729dfd08f52
vinzentschubert <vschubert7@gmail.com>
2026-01-08 16:52:10 -0800

    created configs to store sheet specific consts

---
+++ configurations.py
@@ -0,0 +1,17 @@
+# Hold all constants for portfolio tracker. will allow for me to reflect any changes in the base sheet to be 
+
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+SERVICE_ACCOUNT_FILE = "gcp.json"
+SHEET_ID = '1gm5tL_4mEqldSFhD14PaPOkO_o-ojWbBBJ4Tw863gug'
+
+
+worth_range = 'H2'
+cash_range = 'J10'
+etf_value_range = 'P5'
+stock_value_range = 'F10'
+etf_invested_range = 'N5'
+stock_invested_range = "D10"
+stock_tickers_range = 'A2:A9'
+etf_tickers_range = 'K2:K3'
+stock_holding_range = 'A2:D9'
+etf_holding_range = 'K2:N3'
